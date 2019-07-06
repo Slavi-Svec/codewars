8d9830b4df5a25f49650a93f07b1d7b09b1265f1
Slavi-Svec <slavi.svec@gmail.com>
2019-07-06 10:45:37 +1000

    added function called addition

--- app/js/main.js
+++ app/js/main.js
@@ -12,5 +12,11 @@ document.getElementById('zero').addEventListener('click', () => {
     console.log(zero)
 });
 
-
-
+function addition(testOne, testTwo,testThree) {
+    return testOne  + testTwo  +testThree;   
+  }
+ 
+  console.log(addition(2,1,1))
+  console.log(addition(3,4,1))
+  console.log(addition(5,6,1))
+  console.log(addition(7,8,1))
