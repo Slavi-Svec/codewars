b40314360efca5d9e68831e8ee33d227547b9c88
Slavi-Svec <slavi.svec@gmail.com>
2019-07-06 11:55:07 +1000

    added practice loop

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,28 @@ document.getElementById('izaak').addEventListener('click', () => {
 });
 
 
-document.getElementById('zero').addEventListener('click', () => {
-    var zero = document.getElementById("zero").innerText;
+document.getElementsByClassName('digits')[0].addEventListener('click', () => {
+    var digits = document.getElementsByClassName("digits").innerText;
     document.getElementById('result').innerHTML = zero; 
-    console.log(zero)
+    console.log(digits)
 });
-
 function addition(testOne, testTwo,testThree) {
     return testOne  + testTwo  +testThree;   
   }
  
-  console.log(addition(2,1,1))
-  console.log(addition(3,4,1))
-  console.log(addition(5,6,1))
-  console.log(addition(7,8,1))
+//   console.log(addition(2,1,1))
+//   console.log(addition(3,4,1))
+//   console.log(addition(5,6,1))
+//   console.log(addition(7,8,1))
+
+  var arrayExample = ['heelo', 'whatver', 100, 'fasdfdf'];
+  for (var i = 0; i < arrayExample.length; i++) {
+    console.log('this is the index', i, '   ', 'this is the value', arrayExample[i]);
+  }
+
+  console.log(arrayExample.length)
+  console.log(arrayExample[3])
+
+  
+
+  
