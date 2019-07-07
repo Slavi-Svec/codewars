b8cf673752fb085feef318a169e915a6236a5708
Troy Maeder <Troy.Maeder@oddschecker.com>
2019-07-07 19:40:20 +0100

    Add whichever digit is clicked to the result div

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,18 @@
 // all of our js goes here
+
+//TODO: Get familiar with arrays and for loops and also look into why to use const and let for variables
+// link for const and let explanation https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75
+
+// We get all the digit elements and save them to a variable named "digitElements"
 const digitElements = document.querySelectorAll('.digit');
+
+// Line 12 loop over all the digit elements and add event listeners to them to listen for when they've been clicked
+// On line 14 we add the clicked digit to a variable named "clickedDigit"
+// On line 15 when any digit is clicked we then add the digit to the element with a class name of  "result"
 for (let i = 0; i < digitElements.length; i++) {
     document.getElementsByClassName('digit')[i].addEventListener('click', () => {
-        console.log('the following digit has been clicked', document.getElementsByClassName('digit')[i].innerText);
+        const clickedDigit = document.getElementsByClassName('digit')[i].innerText;
+        document.getElementsByClassName('result')[0].innerText = clickedDigit;
     });
 }
 
@@ -14,10 +24,10 @@ function addition(testOne, testTwo,testThree) {
 //   console.log(addition(5,6,1))
 //   console.log(addition(7,8,1))
 
-var arrayExample = ['heelo', 'whatver', 100, 'fasdfdf'];
-for (var i = 0; i < arrayExample.length; i++) {
+const arrayExample = ['heelo', 'whatver', 100, 'fasdfdf'];
+for (let i = 0; i < arrayExample.length; i++) {
     console.log('this is the index', i, '   ', 'this is the value', arrayExample[i]);
 }
 
-console.log(arrayExample.length)
-console.log(arrayExample[3])
+console.log(arrayExample.length);
+console.log(arrayExample[3]);
