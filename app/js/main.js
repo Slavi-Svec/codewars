3add632768d2b5a4a9e1a1d06da094c7b9f44bb0
Troy Maeder <Troy.Maeder@oddschecker.com>
2019-07-28 04:00:11 +0100

    Remove code with breaking character

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,8 @@
 const digits = document.getElementsByClassName('digit');
 
-for (let i = 0; i < digits.lenght; i++) {
- digits[i].addEventListener("click", function() {
-    const currentDigit = document.getElementsByClassName('digit')[i].innerHTML;
-    document.getElementsByClassName("calc-display-val")[0].innerHTML = currentDigit;
+for (let i = 0; i < digits.length; i++) {
+  digits[i].addEventListener('click', () => {
+    const clickedDigit = document.getElementsByClassName('digit')[i].innerText;
+    document.getElementsByClassName('calc-display-val')[0].innerText = clickedDigit;
  });
 }
-
-
-
-// const digits = document.getElementsByClassName('digit');
-
-// for (let i = 0; i < digits.length; i++) {
-//   digits[i].addEventListener('click', () => {
-//     const clickedDigit = document.getElementsByClassName('digit')[i].innerText;
-//     document.getElementsByClassName('calc-display-val')[0].innerText = clickedDigit;
-//  });
-// }
