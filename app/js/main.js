5ee9a01ca9f3fdefdb5f0db110c737ff6645a8d5
Troy Maeder <Troy.Maeder@oddschecker.com>
2019-08-15 23:25:40 +0100

    Add calc

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,15 @@
 const digits = document.getElementsByClassName('digit');
-
+const addition = document.getElementById('calc-plus');
+// this is the variable we store the number the user types
+let calc = '';
 for (let i = 0; i < digits.length; i++) {
   digits[i].addEventListener('click', () => {
-    const clickedDigit = document.getElementsByClassName('digit')[i].innerText;
-    document.getElementsByClassName('calc-display-val')[0].innerText = clickedDigit;
+    clickedDigit = document.getElementsByClassName('digit')[i].innerText;
+    calc += clickedDigit;
+    const result = document.getElementsByClassName('calc-display-val')[0].innerText = calc;
  });
 }
+
 
 const clear = document.getElementById('calc-clear');
 clear.addEventListener('click', () => {
